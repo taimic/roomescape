3f479df62e2f52b95a46c5726fed6e05190ed815
Taimic <lorenz.krautgartner@gmail.com>
2016-12-10 13:11:33 +0100

    first prototype

---
+++ Assets/PlaySoundOnEnable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnEnable : MonoBehaviour {
+
+    public AudioSource source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray))
+                {
+                    source.Play();
+                }
+            }
+        }
+	}
+
+    private void OnEnable()
+    {
+        //source.Play();
+    }
+
+    private void OnDisable()
+    {
+        source.Stop();
+    }
+}
