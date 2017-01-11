c87ffc0397a5a1ee1d7a7f3fd470ae8b6cd0e87a
Taimic <lorenz.krautgartner@gmail.com>
2017-01-11 08:44:41 +0100

    bup

---
+++ Assets/StartDeviceCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDeviceCamera : MonoBehaviour {
+
+	public WebCamTexture webcamTexture = null;
+	public bool isVisible = true;
+	public GameObject stencil; 
+
+	// Use this for initialization
+	void Start () {
+		webcamTexture = new WebCamTexture ();
+		webcamTexture.requestedHeight = 1280;
+		webcamTexture.requestedWidth = 720;
+		GetComponent<Renderer>().material.mainTexture  = webcamTexture;
+		webcamTexture.Play ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect(100, 100, 200, 200), "press me!")) {
+			if (isVisible){
+				stencil.SetActive(false);
+				isVisible=false;
+			}
+			else {
+				stencil.SetActive(true);
+				isVisible = true;
+			}
+		}	
+		
+	}
+}
---
+++ Assets/displayButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayButton : MonoBehaviour {
+
+	public bool isVisible = true;
+	public GameObject stencil; 
+	public float slider = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnGUI() {
+		slider = GUI.HorizontalSlider(new Rect(50,50,250,80), slider, 0.0f, 1.0f);
+		stencil.GetComponent<Renderer>().material.color = new Color(1,1,1, slider);
+
+		if (GUI.Button (new Rect(50, 150, 150, 100), "display stencil")) {
+			if (isVisible){
+				stencil.GetComponent<MeshRenderer> ().enabled = false;
+				stencil.SetActive(false);
+				isVisible=false;
+			}
+			else {
+				stencil.GetComponent<MeshRenderer> ().enabled = true;
+				stencil.SetActive(true);
+				isVisible = true;
+			}
+		}	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
